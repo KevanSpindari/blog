add5f33f0e577c0ef612fc6627826de675398364
KevanSpindari <48350770+KevanSpindari@users.noreply.github.com>
2020-06-29 16:15:43 +0200

    changes

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -13,6 +13,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegisterType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
